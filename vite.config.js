15a1e009d5e25fabb194419d5340bd32d72f2dbe
ahmedsamerkamal <ahmedsamer9746313@gmail.com>
2025-12-09 05:16:00 +0200

    Fix config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // استبدل 'project-zero' بالاسم اللي سميته للريبو بتاعك بالظبط
-  base: '/project-zero/', 
+  base: '/', // خليه كده (شرطة مايلة بس)
 })
